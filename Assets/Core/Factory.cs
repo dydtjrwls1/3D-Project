94b52c9ccdee471d5bc28426bb6ffd78baee2f7a
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-08-22 13:17:38 +0900

    Add "Core" directory, Install Input System

---
+++ Assets/Core/Factory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : SingleTon<Factory>
+{
+
+    protected override void OnInitialize()
+    {
+    }
+
+}
