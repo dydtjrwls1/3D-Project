09ba6b46e41c8018108c2fbab107ca77b2c74360
bamdal <59675426+dydtjrwls1@users.noreply.github.com>
2024-08-30 10:45:11 +0900

    2024-08-30-(2)
    Camera Following

--- Assets/Scripts/Common/SavePoint.cs
+++ Assets/Scripts/Common/SavePoint.cs
@@ -6,13 +6,39 @@ public class SavePoint : MonoBehaviour
 {
     bool isActivate = false;
 
+    MeshRenderer flag_MeshRenderer;
+
+    public float duration = 1.0f;
+
+    private void Awake()
+    {
+        Transform child = transform.GetChild(0);
+        flag_MeshRenderer = child.GetComponent<MeshRenderer>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !isActivate)
         {
             isActivate = true;
+            StartCoroutine(Activate());
+
             Player player = other.GetComponent<Player>();
             player.CurrentSavePoint = transform.position;
+
         }    
+    }
+
+    IEnumerator Activate()
+    {
+        float time = 0.0f;
+        Material flag_Meterial = flag_MeshRenderer.material;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            flag_Meterial.color = Color.Lerp(flag_Meterial.color, Color.blue, time / duration);
+            yield return null;
+        }
     }
 }
